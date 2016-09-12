1fb6cf00f74372cd0a6e6302a3a3ab251988dcee
Jeff Rogers <jrogers@topologyinc.com>
2016-09-12 18:22:21 -0400

    Adjust android build scripts

--- scripts/android_build_lib.py
+++ scripts/android_build_lib.py
@@ -20,7 +20,12 @@ def get_ndk_dir():
     return get_default_ndk_dir()
 
 def get_default_ndk_dir():
-    return os.path.join(build_lib.get_dev_dir(), "android-ndk-r12")
+    if "ANDROID_HOME" in os.environ:
+        ndk_dir = os.path.join(os.environ["ANDROID_HOME"], "ndk-bundle")
+        if os.path.exists(ndk_dir):
+            return ndk_dir
+
+    return ""
 
 def get_toolchain_dir(arch):
     return os.path.join(build_lib.get_dev_dir(), "android-toolchain-" + arch)
@@ -73,48 +78,11 @@ def get_cmake_toolchain():
 def get_platform(arch):
     return "android-21"
 
-def install_ndk():
-    os.chdir(build_lib.get_dev_dir())
-    ndk_name = "android-ndk-r12"
-    install_prefix = "android-toolchain"
-    if os.path.exists(ndk_name):
-        return 0
-
-    # Fetch the NDK
-    host_os_map = { "linux2" : "linux", "darwin" : "darwin", "win32"  : "windows"}
-    host_os = host_os_map.get(sys.platform, None)
-    if host_os is None:
-        return 1
-
-    host_arch = platform.machine()
-    ndk_file_prefix = ndk_name + "-" + host_os + "-" + host_arch
-    ndk_file = ndk_file_prefix + ".zip"
-
-    # Download and unzip the NDK
-    r = build_lib.run_command("wget http://dl.google.com/android/repository/" + ndk_file + " -O " + ndk_file)
-    if r != 0:
-        return r
-
-    r = build_lib.run_command("unzip " + ndk_file)
-    if r != 0:
-        return r
-
-    # Remove the zip file
-    os.remove(ndk_file)
-
-    # Clean up any standalone toolchains
-    if os.path.exists(install_prefix):
-        shutil.rmtree(install_prefix)
-
-    return 0
-
 def create_toolchain(arch):
     ndk_dir = get_ndk_dir()
     if not os.path.exists(ndk_dir):
-        print "\nAndroid NDK not found. Installing it...\n"
-        r = install_ndk()
-        if r != 0:
-            return r
+        print "\nAndroid NDK not found. Cannot continue.\n"
+        return 1
 
     toolchain_dir = get_toolchain_dir(arch)
 
